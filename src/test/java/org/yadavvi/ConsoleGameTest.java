d16845148001ceb5250d1bf45d490d63485ab565
Vishal Yadav <yadavvi91@gmail.com>
2023-09-09 11:46:17 +0530

    Some more test cases

--- src/test/java/org/yadavvi/ConsoleGameTest.java
+++ src/test/java/org/yadavvi/ConsoleGameTest.java
@@ -19,6 +19,10 @@ class ConsoleGameTest {
         return readFile("src/test/resources/correct_word.txt");
     }
 
+    private String getIncorrectWordString() throws IOException {
+        return readFile("src/test/resources/incorrect_word.txt");
+    }
+
     @Test
     void test_correctWord() throws IOException {
         String str = "m\ne\na\nt\nb\na\nl\nl\n";
@@ -40,4 +44,28 @@ class ConsoleGameTest {
         // System.out.println(actualResponse);
         assertEquals(expectedResponse, actualResponse);
     }
+
+    @Test
+    void test_incorrectWord() throws IOException {
+        String str = "m\ns\no\np\nb\nj\nl\nl\ni\nk\n";
+        InputStream in = new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(outputStream);
+
+        UI ui = new ConsoleUI(in, out);
+        Dictionary dictionary = new FixedDictionary();
+        Game game = new ConsoleGame(ui, dictionary);
+
+        // test that when the string input is `meatball`, the output is the thing printed.
+        game.play();
+
+        String actualResponse = outputStream.toString(StandardCharsets.UTF_8);
+        String expectedResponse = getIncorrectWordString();
+
+        // System.out.println(actualResponse);
+        assertEquals(expectedResponse, actualResponse);
+    }
+
+
 }

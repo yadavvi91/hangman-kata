e4e0015c17d844a2bf888b75151066cd4f62533f
Vishal Yadav <yadavvi91@gmail.com>
2023-09-09 11:33:16 +0530

    Merge else-if

--- src/main/java/org/yadavvi/ConsoleGame.java
+++ src/main/java/org/yadavvi/ConsoleGame.java
@@ -33,26 +33,24 @@ public class ConsoleGame implements Game {
 
             if (isAlreadyGuessed(lettersUsed, guess)) {
                 ui.showAlreadyGuessed(guesses);
+            } else if (isAlreadyGuessed(matched, guess)) {
+                lettersUsed += guess;
+                String newGuesses = "";
+                for (int i = 0; i < word.length(); ++i) {
+                    if (word.charAt(i) == guess) {
+                        newGuesses += guess + " ";
+                    } else {
+                        newGuesses += mid(guesses, i * 2, 2);
+                    }
+                }
+                guesses = newGuesses;
+                matched = matched.replace(guess, '_');
             } else {
-                if (isAlreadyGuessed(matched, guess)) {
-                    lettersUsed += guess;
-                    String newGuesses = "";
-                    for (int i = 0; i < word.length(); ++i) {
-                        if (word.charAt(i) == guess) {
-                            newGuesses += guess + " ";
-                        } else {
-                            newGuesses += mid(guesses, i * 2, 2);
-                        }
-                    }
-                    guesses = newGuesses;
-                    matched = matched.replace(guess, '_');
-                } else {
-                    lettersUsed += guess;
-                    badGuessCount++;
-                    if (badGuessCount == 6) {
-                        ui.showGallows(badGuessCount);
-                        break;
-                    }
+                lettersUsed += guess;
+                badGuessCount++;
+                if (badGuessCount == 6) {
+                    ui.showGallows(badGuessCount);
+                    break;
                 }
             }
             if (isMatched(word, matched)) {
